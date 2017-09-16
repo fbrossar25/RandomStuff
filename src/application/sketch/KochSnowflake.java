c82cee8f7833103105174772a535768c50018897
Unknown <florian.brossard@edu.univ-fcomte.fr>
2017-09-16 09:43:14 +0200

    Preparing the Koch Snowflake

---
+++ src/application/sketch/KochSnowflake.java
@@ -0,0 +1,28 @@
+package application.sketch;
+
+import application.gui.canvas.SketchCanvas;
+
+public class KochSnowflake extends Sketch {
+
+	@Override
+	public void setup(SketchCanvas canvas) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(SketchCanvas canvas) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void draw(SketchCanvas canvas) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static class Segment{
+		double xi =0.0, xf = 0.0, yi = 0.0, yf = 0.0;
+	}
+}
