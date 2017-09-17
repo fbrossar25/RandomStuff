57b824df03679df2f61c283e458ec50c68cd50bf
Unknown <florian.brossard@edu.univ-fcomte.fr>
2017-09-17 10:52:39 +0200

    redraw on size change without updating

--- src/application/gui/canvas/SketchCanvas.java
+++ src/application/gui/canvas/SketchCanvas.java
@@ -22,8 +22,8 @@ public class SketchCanvas extends AutoUpdateCanvas {
     public SketchCanvas(Sketch sketch) {
         ctx = getGraphicsContext2D();
         // Redraw when size changes.
-        widthProperty().addListener(evt -> draw());
-        heightProperty().addListener(evt -> draw());
+        widthProperty().addListener(evt -> handleSizeChange());
+        heightProperty().addListener(evt -> handleSizeChange());
         clear();
         setSketch(sketch);
     }
@@ -48,6 +48,12 @@ public class SketchCanvas extends AutoUpdateCanvas {
         if (sketch == null)
             return;
         sketch.update(this);
+        sketch.draw(this);
+    }
+
+    private void handleSizeChange() {
+        if (sketch == null)
+            return;
         sketch.draw(this);
     }
 
@@ -93,77 +99,100 @@ public class SketchCanvas extends AutoUpdateCanvas {
         }
     }
 
+    @Override
     public void setBackgroundColor(Color backgroundColor) {
         this.backgroundColor = (backgroundColor == null) ? Color.TRANSPARENT : backgroundColor;
     }
 
+    @Override
     public Color getBackgroundColor() {
         return backgroundColor;
     }
 
+    @Override
     public Paint getFill() {
         return fill;
     }
 
+    @Override
     public Paint getStroke() {
         return stroke;
     }
 
+    @Override
     public void setStroke(int gray) {
         setStroke(gray, gray, gray);
     }
 
+    @Override
     public void setStroke(int r, int g, int b) {
         stroke = Color.rgb(r % 256, g % 256, b % 256);
         ctx.setStroke(stroke);
     }
 
+    @Override
     public void setFill(int gray) {
         setFill(gray, gray, gray);
     }
 
+    @Override
     public void setFill(int r, int g, int b) {
         fill = Color.rgb(r % 256, g % 256, b % 256);
         ctx.setFill(fill);
     }
 
+    @Override
     public void fillRect(double x, double y, double w, double h) {
         ctx.fillRect(x, y, w, h);
     }
 
+    @Override
     public void strokeRect(double x, double y, double w, double h) {
         ctx.strokeRect(x, y, w, h);
     }
 
+    @Override
     public void fillText(Object text, double x, double y) {
         ctx.fillText(text.toString(), x, y);
     }
 
+    @Override
     public void strokeText(Object text, double x, double y) {
         ctx.strokeText(text.toString(), x, y);
     }
 
+    @Override
     public void fillPoint(int x, int y) {
         ctx.fillRect(x, y, 1, 1);
     }
 
+    @Override
     public void strokePoint(int x, int y) {
         ctx.strokeRect(x, y, 1, 1);
     }
 
+    @Override
     public void image(Image img) {
         ctx.drawImage(img, (getWidth() / 2) - (img.getWidth() / 2), (getHeight() / 2) - (img.getHeight() / 2));
     }
 
+    @Override
     public void image(Image img, int x, int y) {
         ctx.drawImage(img, x, y);
     }
 
+    @Override
     public void image(Image img, int x, int y, int w, int h) {
         ctx.drawImage(img, x, y, w, h);
     }
 
+    @Override
     public void line(double x1, double y1, double x2, double y2) {
         ctx.strokeLine(x1, y1, x2, y2);
+    }
+
+    @Override
+    public GraphicsContext getContext() {
+        return ctx;
     }
 }
