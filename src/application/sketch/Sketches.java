3ab680f0ed4c823236fffa4602bc9078280969d2
Unknown <florian.brossard@edu.univ-fcomte.fr>
2017-09-17 10:52:17 +0200

    updated sketch's sub package

--- src/application/sketch/Sketches.java
+++ src/application/sketch/Sketches.java
@@ -1,11 +1,18 @@
 package application.sketch;
 
 import application.gui.dialogs.ExceptionDialog;
+import application.sketch.fractal.KochSnowflake;
+import application.sketch.fractal.RandomFractal;
+import application.sketch.noise.ImprovedPerlinNoiseCloud;
+import application.sketch.noise.OpenSimplexNoiseCloud;
+import application.sketch.random.RandomCount;
+import application.sketch.random.RandomGrayShades;
 
 public enum Sketches {
     RANDOM_COUNT(RandomCount.class, "Random count"), RANDOM_GRAY_SHADES(RandomGrayShades.class, "Random Gray Shades"), SIMPLEX_CLOUD(
             OpenSimplexNoiseCloud.class, "OpenSimplex Noise Gray Shades"), IMPROVED_PERLIN_CLOUD(ImprovedPerlinNoiseCloud.class,
-                    "Improved Perlin Noise Gray Shades"), KOCH_SNOWFLAKE(KochSnowflake.class, "Koch Snowflake");
+                    "Improved Perlin Noise Gray Shades"), KOCH_SNOWFLAKE(KochSnowflake.class,
+                            "Koch Snowflake"), RANDOM_FRACTAL(RandomFractal.class, "Random Fractal");
 
     private final Class<? extends Sketch> sketchClass;
     private String                        display;
